f668b8bd2f9ea7da86a09ea5fc3a74e2681fa2b8
Mateusz <mateopolanski@gmail.com>
2022-02-22 12:02:27 +0100

    ToDo

--- src/test/java/APIStatusTest.java
+++ src/test/java/APIStatusTest.java
@@ -7,7 +7,7 @@ public class APIStatusTest {
 
 
     String statusOK = "HTTP/1.1 200 OK";
-    String jsonValidation = "{\"status\":\"OK\"}";
+    String jsonValidation = "{\"status\":\"OK1\"}";
     
     @Test
     public void getApiServiceStatusGWT(){
@@ -18,6 +18,31 @@ public class APIStatusTest {
                 .statusCode(200).statusLine(statusOK).assertThat().body(equalTo(jsonValidation));
 
     }
+/*
+    singleton/ builder
+
+    main/java - konfiguracja np docker, wrappery, singletony
+    tworzenie payloada w main
+            ++++++++++
+    w tescie test-> validatory
+    zmienne w pakiecie w klasie test
+    endpointy w CONST
+    kwestia headerow w rest assured
+    do pakietow wyrzucic np DataGenerator
+    model payload'u adekwatna do tego co robi w pakiecie np model,
+    dynamicznie wyciaganie z odpowiedzi asercji np "Jas Fasola",
+
+    -wszystkie testy, wszystko zeby dzialalo jako wzorzec w folderach, pakietach
+    -> branch framework ===>
+    -> testy najpierw w rest asssurdzie,
+    -> pozniej singleton jako refactor kodu,
+
+    ->target Mon
+
+
+ */
+/*
+
 
     /*
     2.
